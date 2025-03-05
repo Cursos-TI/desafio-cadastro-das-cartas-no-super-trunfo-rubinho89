f9410b396fd987a684b41f70cb36065a5ff51802
rubinho89 <rubenspinheiro2009@gmail.com>
2025-03-05 15:32:42 +0000

    Criei Cartas

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -3,7 +3,7 @@
 //Variavel primeira carta:
 
 
-int main() {
+int main(){
 
     int populacao;
     float area;
@@ -11,7 +11,33 @@ int main() {
     char nome[50];
     int pontosturisticos;
     char estado;
-    char codigo[50];
+    char codigo[4];
+
+    printf("Digite o estado: \n");
+    scanf("%s", estado);
+
+    printf("Digite o nome da cidade: \n");
+    scanf("%s\n", nome);
+
+    printf("Digite o codigo: \n");
+    scanf("%s\n", codigo);
+
+    printf("Digite a populacao: \n");
+    scanf("%d\n", populacao);
+
+    printf("Digite a area: \n");
+    scanf("%s\n", area);
+
+    printf("Digite o pib: \n");
+    scanf("%s\n", Pib);
+
+    printf("Digite os pontos turisticos: \n");
+    scanf("%s\n", pontosturisticos);
+
+    return 0;
+
+  //segunda carta:
+
 
     printf("Digite o estado: \n");
     scanf("%d\n", estado);
@@ -33,40 +59,6 @@ int main() {
 
     printf("Digite os pontos turisticos: \n");
     scanf("%d\n", pontosturisticos);
-
-
-    //Variavel segunda Carta:
-
-    int populacao;
-    float area;
-    float Pib;
-    char nome[50];
-    int pontosturisticos;
-    char estado;
-    char codigo[50];
-
-    printf("Digite o estado: \n");
-    scanf("%d\n", estado);
-
-    printf("Digite o nome da cidade: \n");
-    scanf("%s\n", nome);
-
-    printf("Digite o codigo: \n");
-    scanf("%s\n", codigo);
-
-    printf("Digite a populacao: \n");
-    scanf("%s\n", populacao);
-
-    printf("Digite a area: \n");
-    scanf("%f\n", area);
-
-    printf("Digite o pib: \n");
-    scanf("%f\n", Pib);
-
-    printf("Digite os pontos turisticos: \n");
-    scanf("%d\n", pontosturisticos);
-
-   
 
     return 0;
 }
